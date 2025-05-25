28fc8c1cb6a337c2574a1729d7cc03982188076e
mackarpov <mackarpov@gmail.com>
2025-05-26 00:51:05 +0300

    HW2 completed

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,8 @@
 package ru.netology
 
 fun main() {
-    val likes = 112
-    val endOfAmount: String = if (likes % 10 == 1) "человеку" else "людям"
+    val likes = 101
+    val endOfAmount: String = if (likes % 10 == 1 && likes % 100 != 11) "человеку" else "людям"
 
     println("Понравилось $likes $endOfAmount.")
 }
